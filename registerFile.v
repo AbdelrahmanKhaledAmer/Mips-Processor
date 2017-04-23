9eee2789f8e28ad16f1c199899605a97404bfbf4
Abdelrahman khaled Amer <Abdelrahman khaled Amer>
2017-04-23 16:35:37 +0200

    bin file and changes

--- registerFile.v
+++ registerFile.v
@@ -7,15 +7,16 @@ module registerFile(clk,read_reg_1, read_reg_2, write_reg, write_data, regWrite,
     wire [31:0] read_data_1, read_data_2;
     reg [31:0] registers[31:0];
    
-
     assign read_data_1 = (read_reg_1==0)? 32'b0 : registers[read_reg_1];
     assign read_data_2 = (read_reg_2==0)? 32'b0 : registers[read_reg_2];
 
 	always @(posedge clk)
 		begin
+			registers[0] <= 0;
 			if(regWrite)
 				begin
-					registers[write_reg] <= write_data;
+					if(write_reg!=0)
+						registers[write_reg] <= write_data;
 				end
 		end
 
---
+++ testBench.v
@@ -0,0 +1,3 @@
+module testBench()
+
+endmodule
